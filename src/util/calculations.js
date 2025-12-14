789a8f05f71e7d742b5bb1ad7913eb389b7de0da
Nischal Neupane <nischalneu007@gmail.com>
2025-12-14 15:38:59 +0530

    Fixed the decimal value to 2 for total shipping cost.

--- src/util/calculations.js
+++ src/util/calculations.js
@@ -3,5 +3,5 @@ export function shippingCost(shipWeight, shipDest) {
 	const shippingRate = DEST_COUNTRY_LIST.find(
 		(item) => item.name.toLowerCase() === shipDest.toLowerCase()
 	).rate;
-	return shipWeight * shippingRate;
+	return parseFloat(shipWeight * shippingRate).toFixed(2);
 }
